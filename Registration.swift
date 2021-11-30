2b2dbbfed9c18affc3c61baa39cab8dd1519e786
Yan <matveev.git@gmail.com>
2021-11-30 21:14:28 +1000

    part2

---
+++ Registration.swift
@@ -0,0 +1,8 @@
+//
+//  Registration.swift
+//  fefuactivity
+//
+//  Created by wsr2 on 30.11.2021.
+//
+
+import Foundation
---
+++ firstB.swift
@@ -0,0 +1,8 @@
+//
+//  firstB.swift
+//  fefuactivity
+//
+//  Created by wsr2 on 27.11.2021.
+//
+
+import Foundation
